e69ecd30e1e0a22ccf35b91a9e92512e05fc56be
Jeremy Bolduc <BoldJe1.work@gmail.com>
2016-05-18 11:17:53 -0400

    Ajout de jade et des vues de bases (N'oubliez pas de faire npm update :^) )

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 var express = require('express');
 var config = require('config');
+var path = require('path');
 
 var app = express();
 
+// Initialisation du rendering engine
+app.set('views', path.join(__dirname, 'src/public/views'));
+app.set('view engine', 'jade');
+
+// Initialisation du répertoire statique
+app.use(express.static(path.join(__dirname, 'src/public/static')));
+
 app.get('/', function (req, res) {
-  res.send('Hello World');
+    res.render('index', {title: "Index", bodyContent: 'Hello World'});
 });
 
-
 app.get('/about', function (req, res) {
-  res.send('We are Jenkings!!!');
+    res.render('index', {title: "About", bodyContent: 'We are Jenkings!!!'});
 });
 
 var port = config.get('server_port');
