4882248093e8edf33934bcdf2a56c7b5a26fda80
Jeremy Bolduc <BoldJe1.work@gmail.com>
2016-05-18 11:51:03 -0400

    Ajouts de tests

---
+++ tests/intgration/testingListAuteurs.js
@@ -0,0 +1,44 @@
+var chai = require('chai'),
+    assert = chai.assert,
+    should = chai.should(),
+    expect = chai.expect;
+
+describe("Jenking", function () {
+    describe("route ?", function () {
+
+        describe("GET", function () {
+
+            context("s'il existe des Auteurs", function () {
+                it("devrait retourner touts les Auteurs dans la BD");
+            });
+
+            context("s'il n'Existe pas de Auteurs", function () {
+                it("devrait afficher un empty state");
+            });
+
+        });
+
+        describe("PUT", function () {
+
+            context("si les  données sont valides", function () {
+                it("devrait rajouter un Auteur dans la bd");
+            });
+
+            context("si les donées ne sont pas valides ...", function () {
+
+                context("... car le nom est null ou indéfinit", function () {
+                    it("devrait pas rajouter un document dans la bd");
+                });
+
+                context("... car le prénom est null ou indéfinit", function () {
+                    it("devrait pas rajouter un document dans la bd");
+                });
+
+                context("... car le nom d'utilisateur est null ou indéfinit", function () {
+                    it("devrait pas rajouter un document dans la bd");
+                });
+
+            });
+        });
+    });
+});
---
+++ tests/intgration/testingListDocuments.js
@@ -0,0 +1,52 @@
+var chai = require('chai'),
+    assert = chai.assert,
+    should = chai.should(),
+    expect = chai.expect;
+
+describe("Jenking", function () {
+    describe("route ?", function () {
+
+        describe("GET", function () {
+
+            context("s'il existe des documents", function () {
+                it("devrait retourner toutes les document dans la BD");
+            });
+
+            context("s'il n'Existe pas de documents", function () {
+                it("devrait afficher un empty state");
+            });
+
+        });
+
+        describe("PUT", function () {
+
+            context("si les  données sont valides", function () {
+                it("devrait rajouter un document dans la bd");
+            });
+
+            context("si les donées ne sont pas valides ...", function () {
+
+                context("... car l'auteur ...", function () {
+
+                    context("N'existe pas", function () {
+                        it("devrait pas rajouter un document dans la bd");
+                    });
+
+                    context("est null ou indéfinit", function () {
+                        it("devrait pas rajouter un document dans la bd");
+                    });
+
+                });
+
+                context("... car le type n'existe pas", function () {
+                    it("devrait pas rajouter un document dans la bd");
+                });
+
+                context("... car le corps est null ou indéfinit", function () {
+                    it("devrait pas rajouter un document dans la bd");
+                });
+
+            });
+        });
+    });
+});
