b9a5f4493680c04d9e39d78be87c5d1d744d149b
Sebastien Russell <russell.sebas@gmail.com>
2016-05-20 08:19:40 -0400

    Remove commit-msg hook

--- scripts/init_git.sh
+++ scripts/init_git.sh
@@ -4,7 +4,5 @@
 # Init git pre-commit hook
 echo "Configuration des hooks";
 ln -s -f ../../scripts/pre-commit.githook.sh .git/hooks/pre-commit;
-ln -s -f ../../scripts/commit-msg.githook.sh .git/hooks/commit-msg;
 
 chmod +x .git/hooks/pre-commit;
-chmod +x .git/hooks/commit-msg;
