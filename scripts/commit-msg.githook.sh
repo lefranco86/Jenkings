8f8a5308f5d312b838584a029249e83c8d487cef
Sebastien Russell <russell.sebas@gmail.com>
2016-05-20 07:55:33 -0400

    Try another way for eslint

--- scripts/commit-msg.githook.sh
+++ scripts/commit-msg.githook.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-files=$(git diff --cached --name-only | grep '\.js$')
+files=$(git diff --cached --name-only | grep '\.jsx\?$')
 
 # Prevent ESLint help message if no files matched
 if [[ $files = "" ]] ; then
   exit 0
 fi
 
-echo $files | xargs eslint
+failed=0
+for file in ${files}; do
+  git show :$file | eslint --stdin --stdin-filename $file
+  if [[ $? != 0 ]] ; then
+    failed=1
+  fi
+done;
 
-rc=$?
-if [[ $rc != 0 ]] ; then
+if [[ $failed != 0 ]] ; then
   echo "ESLint check failed, commit denied"
-  exit $rc
+  exit $failed
 fi
