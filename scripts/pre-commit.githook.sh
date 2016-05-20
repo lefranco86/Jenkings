a67ac99b6f092c183672953f6426317815fad7e0
Sebastien Russell <russell.sebas@gmail.com>
2016-05-20 08:20:20 -0400

    testing another pre-commit hook

--- scripts/pre-commit.githook.sh
+++ scripts/pre-commit.githook.sh
@@ -1,7 +1,14 @@
-#!/bin/bash
+#!/bin/zsh
 # Les commandes sont exécuté dans le répertoire racine de Jenkings
 
 export PATH=/usr/local/bin:$PATH
+
+e=$(eslint .)
+echo "${e}";
+if [[ "$e" != *"0 problems"* ]]; then
+  echo "ERROR: Check eslint hints."
+  exit 1 # reject
+fi
 
 # Lancé les suite de tests
 npm test;
